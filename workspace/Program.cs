240f9ef33ad2281a5109327e54fb89b46e04f9bb
Trent Millar <trent.millar@gmail.com>
2023-08-11 14:50:08 -0600

    committing file, Program.cs

---
+++ workspace/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void PrintMessage()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintMessage();
+        }
+    }
+}
