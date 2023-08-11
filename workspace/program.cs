274b89670b9cac451b59ee562ada81b00e9e4c54
Trent Millar <trent.millar@gmail.com>
2023-08-11 14:50:19 -0600

    committing file, program.cs

---
+++ workspace/program.cs
@@ -0,0 +1,11 @@
+using System;
+
+// This is the entry point for the application
+class Program
+{
+    // This function prints "Hello, World!" when run
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
